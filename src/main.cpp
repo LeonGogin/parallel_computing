ba3c13c38c71a34c9d41f30c2b2759d4867616f8
LeonGogin <leonidgogin@protonmail.com>
2024-10-25 22:58:46 +0200

    Create run_experiments function, use dynamicaly allocated vectors to be able to run experiments with different problem dimension

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,52 @@
 #include "utils.h"
 #include "measurment.h"
 
+template <typename T>
+void run_experiments(int n_trials, std::vector<int> &problem_dimensions, std::vector<Measurment::Measurment> &res)
+{
+
+    double execTime{0};
+
+    for (const int &n : problem_dimensions)
+    {
+        auto *r = new float[n * n];
+        auto *d = new float[n * n];
+
+        utils::generate_random_floats<float>(d, n * n, 1.5, 5.5);
+
+        // zero version
+        for (int i = 0; i < n_trials; ++i)
+        {
+            execTime += utils::funcTime<T>(V0::step, r, d, n);
+        }
+        res.emplace_back("V0", n, 1, execTime / n_trials);
+
+        // first version
+        execTime = 0;
+        for (int i = 0; i < n_trials; ++i)
+        {
+            execTime += utils::funcTime<T>(V1::step, r, d, n);
+        }
+
+        res.emplace_back("V1", n, 4, execTime / n_trials);
+
+        delete[] r;
+        delete[] d;
+    }
+}
+
 int main()
 {
-    double execTime{0};
-    constexpr int n = 500;
-    constexpr int n_trial = 1;
 
+    constexpr int n_trial = 10;
     std::vector<Measurment::Measurment> res;
-
-    std::array<float, n * n> d{
-        utils::generateRandomFloats<n * n>(1.5, 5.5)};
-    std::array<float, n * n> r;
-
-    for (int i = 0; i < n_trial; ++i)
+    std::vector<int> problem_dimensions;
+    for (int i = 10; i < 510; i += 10)
     {
-        execTime += utils::funcTime<utils::ms>(V0::step, r.begin(), d.begin(), n);
+        problem_dimensions.push_back(i);
     }
 
-    res.emplace_back("V0", n, 1, execTime / n_trial);
-
-    std::cout << "V0" << std::endl;
-    std::cout << "Average execution time of function: " << execTime / n_trial << " milliseconds" << std::endl;
-
-    execTime = 0;
-    for (int i = 0; i < n_trial; ++i)
-    {
-        execTime += utils::funcTime<utils::ms>(V1::step, r.begin(), d.begin(), n);
-    }
-    
-    res.emplace_back("V1", n, 4, execTime / n_trial);
-    std::cout << "V1" << std::endl;
-    std::cout << "Average execution time: " << execTime / n_trial << " milliseconds" << std::endl;
+    run_experiments<utils::mcs>(n_trial, problem_dimensions, res);
 
     utils::write_csv(res);
 
